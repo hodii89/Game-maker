0799029b5624f4bfccd80c449b23471921b36735
hodii89 <hodii.gaming@gmail.com>
2022-12-01 17:04:00 +0100

    highscore i particle

--- Assets/Scripts/UIScripts/GameManager.cs
+++ Assets/Scripts/UIScripts/GameManager.cs
@@ -8,9 +8,11 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     public Text scoreText;
+    public Text HighscoreText;
     //public Text Highscore;
 
-        int score = 0;
+    int score = 0;
+    int Highscore = 0;
 
     private void Awake()
     {
@@ -20,12 +22,19 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Start()
     {
+        Highscore = PlayerPrefs.GetInt("Highscore", 0);
         scoreText.text = score.ToString() + " CREDITS";
+        HighscoreText.text = "Highscore " + Highscore.ToString();
     }
     public void AddPoint() 
     {
         score += 1;
         scoreText.text = score.ToString() + " CREDITS";
+        if( Highscore < score)
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+        }
+
     }
    
 }
